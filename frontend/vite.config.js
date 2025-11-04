f04bd22dfe04a6a6aeeb45767ffdfe2ca0f1bfa6
Ramla-Eman <ramla.eman.ra@gmail.com>
2025-11-04 14:22:18 +0500

    set error of fronetnd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [
+          ["babel-plugin-react-compiler", {}]
+        ],
       },
     }),
     tailwindcss(),
